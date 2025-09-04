465608a687ea2a5ce1b4260ffbe71d09d4bd3587
Essien <essienetim4002@gmail.com>
2025-09-04 03:22:54 +0200

    add hosts

--- Staking/settings.py
+++ Staking/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['staking-dko0.onrender.com','carlsbergstaking.online']
+
+ALLOWED_HOSTS = [
+    "carlsbergstaking.online",
+    "www.carlsbergstaking.online",
+    "staking-dko0.onrender.com",
+]
 
 
 # Application definition
@@ -175,10 +180,8 @@ ADMIN_NOTIFICATION_EMAIL = os.getenv("ADMIN_NOTIFICATION_EMAIL")
 
 AUTH_USER_MODEL = 'authentication.User'
 
-
 CSRF_TRUSTED_ORIGINS = [
+    "https://carlsbergstaking.online",
+    "https://www.carlsbergstaking.online",
     'https://staking-dko0.onrender.com',
-    'https://www.carlsbergstaking.online',
-    'httsp://carlsbergstaking.online',
 ]
-
